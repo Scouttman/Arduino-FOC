e7e6c20b8f5a4b6e86322fc596cd95fc67791d28
scouttman <sctt.frsr@gmail.com>
2021-02-08 07:24:13 +1100

    using LPF and chagned the order of dq0 transform

--- src/sensors/CurrentSensor.cpp
+++ src/sensors/CurrentSensor.cpp
@@ -28,10 +28,6 @@ void CurrentSensor::init(){
     HAL_OPAMP_Start(&hopamp3);  
 }
 
-int CurrentSensor::test(){
-    return adcBuffer1[0];
-}
-
 float CurrentSensor::get_d(){
     return i_d_filt;
 }
@@ -40,95 +36,42 @@ float CurrentSensor::get_q(){
     return i_q_filt;
 }
 
+int CurrentSensor::test(int i){
+    return adcBuffer1[i];
+}
+
+int CurrentSensor::test2(int i){
+    return adcBuffer2[i];
+}
+
 void CurrentSensor::updateCurrent(float elecAng){
     // memcpy(adcBufferTmp1, adcBuffer1, sizeof(adcBuffer1));
     // memcpy(adcBufferTmp2, adcBuffer2, sizeof(adcBuffer2));
     elecAng = _normalizeAngle(elecAng);
 
-    U_0 = (U_0*63.0+adcBuffer1[2])/64.0;
-    V_0 = (V_0*63.0+adcBuffer2[1])/64.0;
-    W_0 = (W_0*63.0+adcBuffer1[3])/64.0;
+    U_0 = (U_0*63.0+(float)adcBuffer1[2])/64.0; // #TODO replace with LPF
+    V_0 = (V_0*63.0+(float)adcBuffer2[1])/64.0;
+    W_0 = (W_0*63.0+(float)adcBuffer1[3])/64.0;
     //  currentU = convert2current(adcBufferTmp1[0], U_0);
     //  currentV = convert2current(adcBufferTmp2[0], V_0);
     //  currentW = convert2current(adcBufferTmp1[1], W_0);
     //  dq0(elecAng, currentU, currentV, currentW, &f_d, &f_q);
-    dq0(elecAng, adcBuffer1[0]-U_0, adcBuffer2[0]-V_0, adcBuffer1[1]-W_0, &i_d, &i_q);
-    i_d_filt = (i_d_filt*(FILTER_LEN-1)+i_d)/FILTER_LEN;
-    i_q_filt = (i_q_filt*(FILTER_LEN-1)+i_q)/FILTER_LEN;
+    dq0(elecAng, (float)adcBuffer1[0]-U_0, (float)adcBuffer2[0]-V_0, adcBuffer1[1]-W_0, &i_d, &i_q);
+    // i_d_filt = (i_d_filt*(FILTER_LEN-1)+i_d)/FILTER_LEN;
+    // i_q_filt = (i_q_filt*(FILTER_LEN-1)+i_q)/FILTER_LEN;
+    i_d_filt = LPF_d(i_d);
+    i_q_filt = LPF_q(i_q);
 }
 
 // From mit mini cheeta
-void CurrentSensor::dq0(float theta, float a, float b, float c, float *d, float *q){
+void CurrentSensor::dq0(float theta, float a, float c, float b, float *d, float *q){
   /// DQ0 Transform ///
   ///Phase current amplitude = lengh of dq vector///
   ///i.e. iq = 1, id = 0, peak phase current of 1///
   
   float cf = _cos(theta);
   float sf = _sin(theta);
-  
+
   *d = 0.6666667*(cf*a + (_SQRT3_2*sf-.5f*cf)*b + (-_SQRT3_2*sf-.5f*cf)*c);   ///Faster DQ0 Transform
   *q = 0.6666667*(-sf*a - (-_SQRT3_2*cf-.5f*sf)*b - (_SQRT3_2*cf-.5f*sf)*c);       
-}
-
-/**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void OG_MX_ADC1_Init(void)
-{
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
-  ADC_MultiModeTypeDef multimode = {0};
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-  /** Common config 
-  */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV256;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.GainCompensation = 0;
-  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  hadc1.Init.LowPowerAutoWait = DISABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.DMAContinuousRequests = ENABLE;
-  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
-  hadc1.Init.OversamplingMode = DISABLE;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure the ADC multi-mode 
-  */
-  multimode.Mode = ADC_MODE_INDEPENDENT;
-  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure Regular Channel 
-  */
-  sConfig.Channel = ADC_CHANNEL_12;
-  sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
-  sConfig.SingleDiff = ADC_SINGLE_ENDED;
-  sConfig.OffsetNumber = ADC_OFFSET_NONE;
-  sConfig.Offset = 0;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
-
 }
--- src/sensors/CurrentSensor.h
+++ src/sensors/CurrentSensor.h
@@ -4,9 +4,12 @@
 #define ADC_BUF_LEN_1 4
 #define ADC_BUF_LEN_2 2
 #define FILTER_LEN 128
+// low pass filter velocity
+#define DEF_CUR_FILTER_Tf 0.005 //!< default velocity filter time constant
 
 #include "Arduino.h"
 #include "../common/foc_utils.h"
+#include "../common/lowpass_filter.h"
 #include "hardware_api.h"
 
 
@@ -18,9 +21,10 @@ class CurrentSensor{
     
     /**  get current angular velocity (rad/s) */
     void updateCurrent(float elecAng);
-    int test();
     float get_d();
     float get_q();
+    int test(int val);
+    int test2(int val);
     
     float i_d_filt, i_q_filt = 0; 
     float i_d, i_q = 0;
@@ -33,6 +37,8 @@ class CurrentSensor{
     float U_0, V_0, W_0 = 0;
     uint16_t adcBuffer1[ADC_BUF_LEN_1]; // Buffer for store the results of the ADC conversion
     uint16_t adcBuffer2[ADC_BUF_LEN_2]; // Buffer for store the results of the ADC conversion
+    LowPassFilter LPF_d{DEF_CUR_FILTER_Tf};//!<  parameter determining the velocity Lpw pass filter configuration 
+    LowPassFilter LPF_q{DEF_CUR_FILTER_Tf};//!<  parameter determining the velocity Lpw pass filter configuration 
 };
 
 
